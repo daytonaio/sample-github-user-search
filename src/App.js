c1b5b61755c833021bdcf80e6a7b874a625a9184
Prayag Raj Acharya <prayagacharya.raj@gmail.com>
2024-12-17 22:32:38 +0545

    Added all files
    Signed-off-by: Prayag Raj Acharya <prayagacharya.raj@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
+import Footer from "./components/layout/Footer";
+import User from "./components/pages/User";
+import Alert from "./components/layout/Alert";
+import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
+
+function App() {
+  return (
+    <GithubProvider>
+      <AlertProvider>
+        <BrowserRouter>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <div className="container mx-auto">
+            <Alert/>
+              <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="/*" element={<NotFound />} />
+                <Route path="/notfound" element={<NotFound />} />
+              </Routes>
+            </div>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </AlertProvider>
+    </GithubProvider>
+  );
+}
+
+export default App;
